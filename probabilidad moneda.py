b2c553f9fca7fe93d99c827a49cb57d392db879e
Kevin Ariza <kevind199505@gmail.com>
2017-08-11 20:47:56 -0500

    -Add Chart to the program

--- probabilidad moneda.py
+++ probabilidad moneda.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib.pyplot as plt
+import numpy as np
 i = 10000
 opciones =  [0, 1]
 contador = 0
@@ -12,11 +14,21 @@ while i != contador:
 	else:
 		cCara += 1
 
-probSello = cSello / i
-probCara = cCara / i
+probSello = (float(cSello) / i) * 100
+probCara = (float(cCara) / i) * 100
 
 resultado = "probabilidad de Sello: " + str(probSello) + "\nProbabilidad de Cara: " + str(probCara)
 resultado2 = "Veces que cayo Sello: " + str(cSello) + "\nVeces que cayo Cara: " + str(cCara)
 
 print(resultado)
 print(resultado2)
+
+titulo = ["% Sello", "% Cara"]
+posicion_y = np.arange(len(titulo))
+unidades = (probSello, probCara)
+plt.barh(posicion_y, unidades, align = "center")
+plt.yticks(posicion_y, titulo)
+plt.xlabel('Valores')
+plt.title("Grafica de Probabilidad al arrojar una moneda")
+plt.show()
+
